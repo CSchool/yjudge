fa9b1e320e8a9a2d967fe0f87fac611863a50f0e
Tsarev Nikita <nikts53@gmail.com>
2019-08-26 14:25:31 +0300

    Removed FORCE_USER and FORCE_GROUP

--- yjudge-runner/config.h
+++ yjudge-runner/config.h
@@ -1,5 +1,5 @@
 #define SUID_GROUP "yj-runner"
 #define N_SYSCALLS 512
 #define INJECTOR_PATH "/usr/local/lib/yjudge-inject.so"
-#define FORCE_USER "nobody"
-#define FORCE_GROUP "nobody"
+//#define FORCE_USER "nobody"
+//#define FORCE_GROUP "nobody"
